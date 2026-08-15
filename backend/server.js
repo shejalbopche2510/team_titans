0cce22c2b839bb0003d54538f6aa256b180290a1
shejalbopche2510 <shejalbopche2510@gmail.com>
2026-08-15 12:52:27 +0530

    Setup backend server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//test route
+app.get("/",(req, res)=>{
+    res.json({
+        message:"server is runing"
+    });
+});
+
+//server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+console.log(`server running on port ${PORT}`);
+})
