2503f5931fe2bb2668dabdac75c75dfdb89719b6
Anushka <anushkadivate7@gmail.com>
2026-08-15 13:33:55 +0530

    Added student pages routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,37 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-
-import AdminLogin from "./admin/AdminLogin";
-import AdminDashboard from "./admin/AdminDashboard";
+import StudentDashboard from "./Student/StudentDashboard";
+import SubmitComplaint from "./Student/SubmitComplaint";
+import MyComplaints from "./Student/MyComplaints";
+import ComplaintDetails from "./Student/ComplaintDetails";
+import StudentProfile from "./Student/StudentProfile";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
       <Routes>
-        <Route path="/" element={<AdminLogin />} />
+        <Route path="/" element={<StudentDashboard />} />
+
+        <Route path="/student" element={<StudentDashboard />} />
 
         <Route
-          path="/admin/dashboard"
-          element={<AdminDashboard />}
+          path="/student/submit"
+          element={<SubmitComplaint />}
+        />
+
+        <Route
+          path="/student/complaints"
+          element={<MyComplaints />}
+        />
+
+        <Route
+          path="/student/complaints/:id"
+          element={<ComplaintDetails />}
+        />
+
+        <Route
+          path="/student/profile"
+          element={<StudentProfile />}
         />
       </Routes>
     </BrowserRouter>
