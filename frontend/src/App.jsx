da9e8da1fa9c9fb07e2c15ef19462bfc48b735ef
Purva <ruchijoshi2025@gmail.com>
2026-08-15 12:36:50 +0530

    Add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import AdminLogin from "./admin/AdminLogin";
 import AdminDashboard from "./admin/AdminDashboard";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<AdminLogin />} />
 
