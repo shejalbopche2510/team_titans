d7c1dbfa16dc06614cb89a6cde27de42138489a3
Anushka <anushkadivate7@gmail.com>
2026-08-15 13:43:53 +0530

    Fixed student routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,44 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import StudentDashboard from "./Student/StudentDashboard";
-import SubmitComplaint from "./Student/SubmitComplaint";
-import MyComplaints from "./Student/MyComplaints";
-import ComplaintDetails from "./Student/ComplaintDetails";
-import StudentProfile from "./Student/StudentProfile";
+import StudentDashboard from "./student/StudentDashboard";
+import SubmitComplaint from "./student/SubmitComplaint";
+import MyComplaints from "./student/MyComplaints";
+import ComplaintDetails from "./student/ComplaintDetails";
+import StudentProfile from "./student/StudentProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StudentDashboard />} />
 
+        {/* Student Dashboard */}
+        <Route path="/" element={<StudentDashboard />} />
         <Route path="/student" element={<StudentDashboard />} />
 
+        {/* Submit Complaint */}
         <Route
           path="/student/submit"
           element={<SubmitComplaint />}
         />
 
+        {/* My Complaints */}
         <Route
           path="/student/complaints"
           element={<MyComplaints />}
         />
 
+        {/* Complaint Details */}
         <Route
           path="/student/complaints/:id"
           element={<ComplaintDetails />}
         />
 
+        {/* Student Profile */}
         <Route
           path="/student/profile"
           element={<StudentProfile />}
         />
+
       </Routes>
     </BrowserRouter>
   );
