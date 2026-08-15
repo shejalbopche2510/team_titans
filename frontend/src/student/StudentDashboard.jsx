76ba94cb3be8590e6df0324aba57b7545ae91c75
Anushka <anushkadivate7@gmail.com>
2026-08-15 11:43:31 +0530

    Added Student Dashboard

---
+++ frontend/src/student/StudentDashboard.jsx
@@ -0,0 +1,10 @@
+function StudentDashboard() {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+      <p>Smart Campus Complaint Management System</p>
+    </div>
+  );
+}
+
+export default StudentDashboard;
